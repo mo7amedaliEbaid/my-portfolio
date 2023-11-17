b0dafb09e395d5be889d89b2f85c8cdc054cf46f
Mohamed Ali <mo7amedaliebaid@gmail.com>
2023-11-17 05:56:07 +0200

    update_17_11_2023

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,12 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 import 'app.dart';
-import 'dart:js' as js;
 
 void configureApp() {
   setUrlStrategy(PathUrlStrategy());
 }
 
 Future<void> main() async {
-  js.context.callMethod('disableLandscapeMode');
   await WidgetsFlutterBinding.ensureInitialized();
   configureApp();
 
